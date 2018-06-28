82a5135faf0417e70097356a4c6e78a29028bdd9
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-06-27 22:46:52 -0400

    muestra datos de api en html

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 //let content= document.getElementById("contenedor")
 
-let numberBip=18862002; // variable donde se guarda el numero
+let numberBip=12844501; // variable donde se guarda el numero
 
 function bipNumber(){
-    const link=fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberBip}`)
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberBip}`)
     .then(function(response){
         return response.json()
     })
     
     .then(function(data){ //usar object.keys?
         
-        const infoBip = Object.entries(data) // se puede cambiar por object.keys o object.values 
+        const infoBip = Object.values(data) // se puede cambiar por object.keys o object.values 
+        console.log(infoBip[0])
+        console.log(infoBip[1])
+        console.log(infoBip[2])
+        console.log(infoBip[3])
+
+        let bip1= infoBip[0];
+        document.getElementById("databip").innerHTML ='numero de bip '+ bip1;
+
+        let bip2= infoBip[1];
+        document.getElementById("databip1").innerHTML ='Status '+ bip2;
+
+        let bip3= infoBip[2];
+        document.getElementById("databip2").innerHTML ='saldo '+ bip3;
+
+        let bip4= infoBip[3];
+        document.getElementById("databip3").innerHTML ='fecha de carga '+ bip4;
+       
+
         console.log(infoBip);
         console.log(data);
     })
