98e375c33e94be653a4feb5d31ef9d4dccc9e75c
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-06-28 02:00:58 -0400

    función suma que calcula si tienes saldo para un viaje

--- app.js
+++ app.js
@@ -1,41 +1,63 @@
-
- window.onload = function() {
-
-    document.getElementById("btn").onclick = function()
-     { let numberBip=document.getElementById("bipCard").value;
-    
-
-    
-    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberBip}`)
-    .then(function(response){
-        return response.json();
-    })
-    
-    .then(function(data){ //usar object.keys?
-        
-        const dataBip = Object.values(data) // se puede cambiar por object.keys o object.values 
-       
-        let numberBip= dataBip[0];
-        document.getElementById("databip").innerHTML ='numero de bip '+ numberBip;
-
-        let statusBip= dataBip[1];
-        document.getElementById("databip1").innerHTML ='Status '+ statusBip;
-
-        let amountBip= dataBip[2];
-        document.getElementById("databip2").innerHTML ='saldo '+ amountBip;
-
-        let dayBip= dataBip[3];
-        document.getElementById("databip3").innerHTML ='fecha de carga '+ dayBip;
-       
-    })
-  
-    .catch(function(fail){
-    console.log('fail',fail)
-
-    }) // fin fetch
+let saldoBip;
 
 
-    }}
+window.onload = function getBip() {
+
+    document.getElementById("btn").onclick = function () {
+        let numberBip = document.getElementById("bipCard").value;
+
+        fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${numberBip}`)
+            .then(function (response) {
+                return response.json();
+            })
+
+            .then(function (data) { //usar object.keys?
+
+                const dataBip = Object.values(data) // se puede cambiar por object.keys o object.values 
+
+                let numberBip = dataBip[0];
+                document.getElementById("databip").innerHTML = 'numero de bip ' + numberBip;
+
+                let statusBip = dataBip[1];
+                document.getElementById("databip1").innerHTML = 'Status ' + statusBip;
+
+                let amountBip = dataBip[2];
+                document.getElementById("databip2").innerHTML = 'saldo ' + amountBip;
+                saldoBip= Number(amountBip.replace(/[$,.]+/g,""));
+                console.log(saldoBip);
+
+                let dayBip = dataBip[3];
+                document.getElementById("databip3").innerHTML = 'fecha de carga ' + dayBip;
+
+            })
+
+
+            .then (function calcularValorPasaje(){
+                if (saldoBip > 630){
+                    document.getElementById("alerta").innerHTML="suficiente saldo";
+                    console.log(saldoBip);
+                }else{
+                    document.getElementById("alerta").innerHTML="falta saldo";
+                }
+            
+            }
+
+            )
+
+            .catch(function (fail) {
+                console.log('fail', fail)
+
+            }) 
+            
+            // fin fetch
+
+
+    } // fin llamada boton
+}// fin windows.onload
+
+// para mostrar saldo
+
+
 
 
 
